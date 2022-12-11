85e8ccfa839d99e8b200ad9e8f1acb5f3f582fec
Neil Feris <neil.feris@gmail.com>
2022-12-11 16:26:15 +0200

    mobile fixes

--- pages/_app.js
+++ pages/_app.js
@@ -20,12 +20,16 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = (url) => {
+      console.log("route change", url);
       gtag.pageview(url);
     };
+
     router.events.on("routeChangeComplete", handleRouteChange);
+    
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
+    
   }, [router.events]);
 
   return (
